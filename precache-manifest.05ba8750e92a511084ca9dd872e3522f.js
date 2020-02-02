4c7b0becb90c53305a476fd53ff9e6d80998f83f
Irina Lysenko <irina.lysenko21@gmail.com>
2020-02-02 21:45:13 +0200

    Updates

---
+++ precache-manifest.05ba8750e92a511084ca9dd872e3522f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f9a340ea03a53ba81d6e7a0ec1f6dbe",
+    "url": "/goit-react-hw-05-phonebook/index.html"
+  },
+  {
+    "revision": "af7124bd44878f998a2f",
+    "url": "/goit-react-hw-05-phonebook/static/css/2.34041091.chunk.css"
+  },
+  {
+    "revision": "24aa7ef441fbabd1a05d",
+    "url": "/goit-react-hw-05-phonebook/static/css/main.1f6b1494.chunk.css"
+  },
+  {
+    "revision": "af7124bd44878f998a2f",
+    "url": "/goit-react-hw-05-phonebook/static/js/2.723bec4d.chunk.js"
+  },
+  {
+    "revision": "24aa7ef441fbabd1a05d",
+    "url": "/goit-react-hw-05-phonebook/static/js/main.ae3edbd4.chunk.js"
+  },
+  {
+    "revision": "ba7eecc1a22cea7b0be0",
+    "url": "/goit-react-hw-05-phonebook/static/js/runtime-main.8b23889c.js"
+  }
+]);
